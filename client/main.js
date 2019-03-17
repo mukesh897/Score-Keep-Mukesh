c6bac2de0079611e1fd555bdf8ba256a582bf57a
mukesh <mukesh.bhatia85@gmail.com>
2019-03-18 00:45:35 +0530

    first commit

---
+++ client/main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Meteor} from 'meteor/meteor';
+import {Tracker} from 'meteor/tracker';
+
+import {Players,calculatePlayerPositions} from './../imports/api/players';
+import App from './../imports/ui/App';
+
+
+Meteor.startup(() => {
+  Tracker.autorun(() => {
+    let players = Players.find({},{sort:{score:-1}}).fetch();
+    let title = 'Score Keep';
+    let positionedPlayers = calculatePlayerPositions(players);
+    ReactDOM.render(<App title={title} players={positionedPlayers}/>, document.getElementById('app'));
+  });
+});
